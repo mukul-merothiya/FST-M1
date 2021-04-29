f6fbafcabcc5e46becd9b1835a653d3be2554c3c
mukul-merothiya <57950446+mukul-merothiya@users.noreply.github.com>
2021-04-29 11:34:38 +0530

    Add files via upload

---
+++ java/Activity1.java
@@ -0,0 +1,19 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Car toyota=new Car();
+toyota.Color="Red";
+toyota.make="Toyota";
+toyota.transmission="Automatic";
+toyota.displayCharacteristics();
+toyota.accelerate();
+toyota.brake();
+
+
+
+	}
+
+}
