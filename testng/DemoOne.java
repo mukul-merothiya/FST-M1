91e9593805826b15bd0d4a126bd3ca07c0472df7
mukul-merothiya <57950446+mukul-merothiya@users.noreply.github.com>
2021-04-29 12:15:54 +0530

    Add files via upload

---
+++ testng/DemoOne.java
@@ -0,0 +1,11 @@
+package TestNG_Activities;
+
+import org.testng.annotations.Test;
+
+public class DemoOne {
+  @Test
+  public void f() {
+	  System.out.println("First Test Case of DemoOne.java");
+	  System.out.println("Second Test Case of DemoOne.java");
+  }
+}
---
+++ testng/DemoTwo.java
@@ -0,0 +1,10 @@
+package TestNG_Activities;
+
+import org.testng.annotations.Test;
+
+public class DemoTwo {
+  @Test
+  public void f() {
+	  System.out.println("This is first TC of DemoTwo.java");
+  }
+}
